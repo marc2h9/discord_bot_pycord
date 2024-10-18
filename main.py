272fcac06325d88c9452ee2ae86dd7441a123b6b
marc2h9 <marcverstegen09@gmail.com>
2024-10-18 20:52:32 +0200

    added ping command

--- main.py
+++ main.py
@@ -9,5 +9,9 @@ bot = discord.Bot()
 async def on_ready():
     print(f"{bot.user} is ready and online")
 
+@bot.command(description="Sends bot latency")
+async def ping(ctx):
+    await ctx.respond(f"Pong! Latency is {round(bot.latency)}ms")
+
 # starting bot
 bot.run(os.getenv('TOKEN'))
